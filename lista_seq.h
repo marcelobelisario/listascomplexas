bad358eb1f66afc7c6b859a48e8f89bf886ea30d
Marcelo Augusto Silva Belisário <marcelobeli9@gmail.com>
2023-05-08 01:19:37 -0300

    comentarios

--- lista_seq.h
+++ lista_seq.h
@@ -1,32 +1,71 @@
 #include <stdbool.h>
 
 // TAD lista sequencial 
-
 typedef struct lista Lista_s;
 
 // FUNÇÕES AUXILIARES
+
+/* Cria uma lista sequencial
+Entrada: capacidade da lista
+Saída: endereço de memoria para nova lista criada */
 Lista_s *criar(int capacidade);
 
+/* Informa a capacidade da lista
+Entrada: lista sequencial
+Saída: capacidade da lista */
 int capacidade(Lista_s *x);
 
+/* Informa o tamanho da lista
+Entrada: lista sequencial
+Saída: tamanho da lista */
 int tamanho(Lista_s *x);
 
+/* Verifica se a lista está vazia
+Entrada: lista sequencial
+Saída: true para lista vazia, false caso contrário */
 bool vazia(Lista_s *x);
 
+/* Verifica se a lista está cheia
+Entrada: lista sequencial 
+Saída: true para lista cheia, false caso contrário */
 bool cheia(Lista_s *x);
 
+/* Insere um elemento no começo da lista
+Entrada: lista sequencial e o elemento a inserir
+Saída: true para elemento inserido, false caso contrário */
 bool inserir_inicio(Lista_s *x, int elemento);
 
+/* Remove um elemento do começo da lista
+Entrada: lista sequencial
+Saída: true para primeiro elemento removido, false caso contrário */
 bool remover_inicio(Lista_s *x);
 
+/* Insere um valor no meio da lista
+Entrada: lista sequencial, elemento a inserir e posicao onde inserir o elemento
+Saída: true para elemento inserido, false caso contrário */
 bool inserir_meio(Lista_s *x, int elemento, int posicao);
 
+/* Remove um elemento do meio da lista
+Entrada: lista sequencial e posicao k onde remover o elemento
+Saída: true para elemento removido, false caso contrário */
 bool remover_meio(Lista_s *x, int k);
 
+/* Insere um elemento no fim da lista
+Entrada: lista sequencial e elemento a ser inserido
+Saída: true para elemento inserido, false caso contrário */
 bool inserir_fim(Lista_s *x, int elemento);
 
+/* Remove um elemento do fim da lista
+Entrada: lista sequencial 
+Saída: true para elemento removido, false caso contrário */
 bool remover_fim(Lista_s *x);
 
+/* Informa quantos elementos presentes na lista são negativos
+Entrada: lista sequencial 
+Saída: quantidade de elementos negativos presentes na lista */
 int qnte_impares_lst(Lista_s *l);
 
+/* Exibe a lista
+Entrada: lista sequencial
+Saída: não há */
 void exibir(Lista_s *x);
--- teste_lista.c
+++ teste_lista.c
@@ -4,14 +4,24 @@
 #include <time.h>
 
 int main(){
+    // Define uma seed aleatória através da hora do Windows
     srand(time(NULL));
+    
+    // Cria uma lista sequencial
     Lista_s *lista_nova = criar(30);
+    
+    // Insere um valor aleatório na primeira posição da lista
     inserir_inicio(lista_nova, rand()% 21 -10);
+
+    // Estrutura de repetição para inserir valores aleatórios no final da lista
     for(int i=0; i<25; i++){
         inserir_fim(lista_nova, rand()% 21 -10);
     }
+
+    // Exibe a lista
     exibir(lista_nova);
 
+    // Informa a quantidade de números ímpares presentes na lista
     printf("\n quantidade de numeros impares: %d", qnte_impares_lst(lista_nova));
 
     return 0;
